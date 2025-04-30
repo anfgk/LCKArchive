ba9755a08909328e666d577a5211cae4c8cac151
anfgk <gloria020510@gmail.com>
2025-04-30 20:01:28 +0900

    UI_Edit

--- game/client/src/components/Team.jsx
+++ game/client/src/components/Team.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { teams } from "../assets/team";
 import styled from "styled-components";
 
@@ -24,24 +24,7 @@ const Title = styled.div`
   color: #fff;
   font-size: 50px;
   font-weight: 700;
-  margin-top: 80px;
-`;
-
-const Ul = styled.ul`
-  display: flex;
-  gap: 40px;
-  color: #fff;
-  font-size: 20px;
-  margin: 50px 0;
-  li {
-    padding: 8px 16px;
-    &:hover {
-      padding: 8px 16px;
-      border: 1px solid #fff;
-      border-radius: 30px;
-      cursor: pointer;
-    }
-  }
+  margin: 80px 0;
 `;
 
 const Container = styled.div`
@@ -53,60 +36,48 @@ const Container = styled.div`
 `;
 
 const TeamWrap = styled.div`
-  border: 1px solid #a2a2a2;
-  border-radius: 8px;
   width: 170px;
+  height: 170px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  transition: transform 0.3s ease;
+
+  h2 {
+    color: #fff;
+    font-size: 14px;
+    font-weight: bold;
+    margin-top: 10px;
+  }
+
+  &:hover {
+    cursor: pointer;
+    transform: scale(1.1);
+  }
 `;
 
 const Img = styled.img`
-  width: 100%;
+  width: 100px;
   height: 100px;
-  background: #fff;
-  border-radius: 8px 8px 0 0;
-  padding: 20px;
   object-fit: contain;
-`;
-
-const Member = styled.div`
-  color: #fff;
-  padding: 0 20px 20px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
+  display: block;
+  margin: 0 auto;
+  padding: 10px;
+  background-color: transparent;
 `;
 
 const Team = () => {
-  const [category, setCategory] = useState("all");
-
-  const filterTeams = (category) => {
-    if (category === "all") {
-      return teams;
-    }
-    return teams.filter((team) => team.category === category);
-  };
-
   return (
     <Wrapper>
       <Inner>
-        <Title>2025 LCK Team</Title>
-        <Ul>
-          <li onClick={() => setCategory("all")}>ALL</li>
-          <li onClick={() => setCategory("baron")}>Group Baron</li>
-          <li onClick={() => setCategory("elder")}>Group Elder</li>
-        </Ul>
+        <Title>2025 LCK 참가팀</Title>
         <Container>
-          {filterTeams(category).map((team) => (
+          {teams.map((team) => (
             <div key={team.id}>
               <TeamWrap>
                 <Img src={team.img} alt={team.team} />
                 <h2>{team.Team}</h2>
-                <Member>
-                  <div>Top: {team.top}</div>
-                  <div>Jungle: {team.jungle}</div>
-                  <div>Mid: {team.mid}</div>
-                  <div>AD: {team.ad}</div>
-                  <div>Support: {team.support}</div>
-                </Member>
               </TeamWrap>
             </div>
           ))}
